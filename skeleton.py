9f1817d4f8d664fcf5418582bed5c39c0049635d
AdityaDaflapurkar <adaflapurkar@gmail.com>
2017-05-08 23:48:11 +0530

    Model updated with rgb values as features

--- skeleton.py
+++ skeleton.py
@@ -7,7 +7,6 @@ import math
 from sklearn import svm
 from sklearn.neural_network import MLPClassifier
 import pickle
-from scipy import ndimage
 	
 class Segment(object):
 
@@ -19,7 +18,7 @@ class Segment(object):
                             includes the dataset folder with '/'
             Initialize all your variables here
         """
-	self.model = MLPClassifier(hidden_layer_sizes=(64,32,64),verbose=True)
+	self.model = MLPClassifier(hidden_layer_sizes=(64,32,64))
 
     def train(self):
         """
@@ -27,40 +26,36 @@ class Segment(object):
 
             No return expected
         """
-	print "Collecting images..."
+	print ("Collecting images...")
 	input_images=[]
 	output_images=imread_collection('segmentation/Train_Data/*-mask.jpg')[0:164]
 	for i in xrange(164):
 		input_images.append(imread('segmentation/Train_Data/train-'+str(i)+'.jpg'))
 	x=[]
 	y=[]
-	# mlp = MLPClassifier(hidden_layer_sizes=(50,100,),verbose=True)
 	X=[]
 
 	for k in range(123):
 	# For each image
-		print k
-		img_gray = rgb2gray(input_images[k])
-		b = np.ones((6,6))
-
-		av = ndimage.convolve(img_gray, b, mode='reflect')
-
+		print ("iteration : ",k)
+		img = input_images[k]
 		y = np.array(output_images[k]).flatten()
-		dist=np.empty([128,128])
+		#dist=np.empty([128,128])
+		'''		
 		x=np.empty([128,128,3])
 		n=math.sqrt(((0-63.5)**2)+((0-63.5)**2))
-		for i in xrange(len(dist)):
+		for i in range(len(dist)):
 			for j in xrange(len(dist)):
 			# For each pixel
-				dist[i][j]=math.sqrt(((i-63.5)**2)+((j-63.5)**2))
+				#dist[i][j]=math.sqrt(((i-63.5)**2)+((j-63.5)**2))
 				#print n,"eee"
 				#print dist[i][j],"ddd"
-				dist[i][j]=dist[i][j]/n
+				#dist[i][j]=dist[i][j]/n
 				x[i][j][0]=dist[i][j]
-				x[i][j][1]=img_gray[i][j]
-				x[i][j][2]=av[i][j]
+				x[i][j][1]=img[i][j]
+		'''	
 			
-		x=np.reshape(x,(len(x)*len(x),2))
+		x=np.reshape(img,(len(img)*len(img),3))
 		print x
 		print y
 	 	
@@ -75,11 +70,12 @@ class Segment(object):
 
         """
 
-	img_gray = rgb2gray(image)
+	img = image
 	#y = np.array(output_images[k]).flatten()
-	dist=np.empty([128,128])
-	x=np.empty([128,128,2])
-	n=math.sqrt(((0-63.5)**2)+((0-63.5)**2))
+	#dist=np.empty([128,128])
+	#x=np.empty([128,128,2])
+	#n=math.sqrt(((0-63.5)**2)+((0-63.5)**2))
+	'''	
 	for i in xrange(len(dist)):
 		for j in xrange(len(dist)):
 		# For each pixel
@@ -88,11 +84,11 @@ class Segment(object):
 			#print dist[i][j],"ddd"
 			dist[i][j]=dist[i][j]/n
 			x[i][j][0]=dist[i][j]
-			x[i][j][1]=img_gray[i][j]
-
-	x=np.reshape(x,(len(x)*len(x),3))
-	print x
-	print y
+			x[i][j][1]=img[i][j]
+	'''
+	x=np.reshape(img,(len(img)*len(img),3))
+	#print x
+	#print y
 	p = self.model.predict(x)
 	return np.reshape(p,(128,128))
 	
@@ -113,13 +109,16 @@ class Segment(object):
 	
 
 if __name__ == "__main__":
-	obj = Segment('dataset/')
-	obj.train()
+	#obj = Segment('dataset/')
+	#obj.train()
 	#obj.save_model(name="segment.gz")
 	obj = Segment.load_model()
-	print "Collecting images..."
+	print ("Collecting images...")
 	input_images=[]
-	for i in range(1,5):
+	print ("Done.")
+	for i in range(123,164):
 		input_images.append(imread('segmentation/Train_Data/train-'+str(i)+'.jpg'))
+		
+	for i in range(41):
 		imshow(obj.get_mask(input_images[i]))
 		plt.show()
